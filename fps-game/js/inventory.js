5ddfeec6a208232154a9a8cfcbfe0e2ce2e8d3e7
010101010101101010101010 <master13sy@gmail.com>
2025-12-21 14:12:22 +0900

    Update inventory.js

--- fps-game/js/inventory.js
+++ fps-game/js/inventory.js
@@ -1,1 +1,16 @@
- 
+export const inventory = {
+  scopes: [],
+  equippedScope: null,
+
+  addScope(scope) {
+    if (!this.scopes.includes(scope)) {
+      this.scopes.push(scope);
+    }
+  },
+
+  equip(scope) {
+    if (this.scopes.includes(scope)) {
+      this.equippedScope = scope;
+    }
+  }
+};
