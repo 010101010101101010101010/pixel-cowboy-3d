db84ef792e878b237c9904096f934cdc3df05737
010101010101101010101010 <master13sy@gmail.com>
2025-12-21 13:43:10 +0900

    Update dummy.js

--- fps-game/js/dummy.js
+++ fps-game/js/dummy.js
@@ -1,1 +1,25 @@
- 
+export class Dummy {
+  constructor() {
+    this.hp = 100;
+    this.xOffset = 0;
+    this.damageText = null;
+    this.damageTimer = 0;
+  }
+
+  hit(dmg) {
+    this.hp -= dmg;
+    this.xOffset = -10;
+
+    this.damageText = dmg;
+    this.damageTimer = 30;
+
+    if (this.hp <= 0) {
+      this.hp = 100;
+    }
+  }
+
+  update() {
+    this.xOffset *= 0.8;
+    if (this.damageTimer > 0) this.damageTimer--;
+  }
+}
